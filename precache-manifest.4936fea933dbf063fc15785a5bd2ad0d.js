ab882e89551b2143cbc4130d4a035a1eae8d0d5a
Vigneshwaran <vigneshwaran0310@gmail.com>
2020-05-09 20:50:53 -0400

    Updates

---
+++ precache-manifest.4936fea933dbf063fc15785a5bd2ad0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceb71b53a53204b80dd1",
+    "url": "/my-portfolio/static/css/main.8b85d37e.chunk.css"
+  },
+  {
+    "revision": "ceb71b53a53204b80dd1",
+    "url": "/my-portfolio/static/js/main.ceb71b53.chunk.js"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "4e6a682274bac351a2d5",
+    "url": "/my-portfolio/static/css/2.6faa8fa2.chunk.css"
+  },
+  {
+    "revision": "4e6a682274bac351a2d5",
+    "url": "/my-portfolio/static/js/2.4e6a6822.chunk.js"
+  },
+  {
+    "revision": "0f3832a3c2d056240e52b3fe3ee80eaf",
+    "url": "/my-portfolio/static/media/resume.0f3832a3.pdf"
+  },
+  {
+    "revision": "d6a89e77277ef956f927d9a5586a1a33",
+    "url": "/my-portfolio/index.html"
+  }
+];
